16db99ba5155baaa8cd5609b6e64caa7304850b6
Prasoonjha1 <prasoonjha2000@gmail.com>
2022-08-24 22:48:24 +0530

    Second commit

---
+++ src/Body.js
@@ -0,0 +1,29 @@
+function Body()
+{
+    return(
+        <div>
+        <h1>Fun facts about React</h1>
+        <div>
+            <ol>
+                <li>
+                    Was released in 2013
+                </li>
+                <li>
+                    Was originally created by Jordan Walke
+                </li>
+                <li>
+                    Has well over 100k stars on Github
+                </li>
+                <li>
+                  Is maintained by facebook
+                </li>
+                <li>
+                  Powers thousands of enterprise apps, including mobile apps
+                </li>
+            </ol>
+            </div>
+        </div>
+    )
+}
+
+export default Body
--- src/index.js
+++ src/index.js
@@ -1,83 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom"
-/*function Navbar()
-{
-    return (
-        <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-          <a class="navbar-brand" href="#">Navigation</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-              <ul class="navbar-nav mr-auto">
-                  <li class="nav-item active">
-                      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="nav-link" href="#">Market</a>
-                  </li>
-              </ul>
-          </div>
-        </nav>
-  )
-}
+import './index.css'
+import Header from "./Header"
+import Body from "./Body"
+import Footer from "./Footer"
 
-function MainComp()
-{
-    return (
-            <h1>I am learning, React!</h1>
-            )
-}*/
 
-function Header()
-{
-    return (
-            <header>
-                <nav>
-                    <img src="logo192.png" width="40px"></img>
-                </nav>
-            </header>
-    )
-}
-
-function Footer()
-{
-    return (
-        <footer>
-            <small>© 2022 Apx development. All rights reserved.</small>
-        </footer>
-    )
-}
-
-function Body()
-{
-    return(
-        <div>
-        <h1>Fun facts about React</h1>
-        <div>
-            <ol>
-                <li>
-                    Was released in 2013
-                </li>
-                <li>
-                    Was originally created by Jordan Walke
-                </li>
-                <li>
-                    Has well over 100k stars on Github
-                </li>
-                <li>
-                  Is maintained by facebook
-                </li>
-                <li>
-                  Powers thousands of enterprise apps, including mobile apps
-                </li>
-            </ol>
-            </div>
-        </div>
-    )
-}
-
-function TemporaryName(){
+function App(){
     return(
     <div>
         <Header />
@@ -86,8 +15,6 @@ function TemporaryName(){
     </div>)
 }
 
-//document.getElementById("root").append(JSON.stringify(page))
-
 ReactDOM.render(
-                <TemporaryName/>,
+                <App/>,
               document.getElementById("root"))
