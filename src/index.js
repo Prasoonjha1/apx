0f14db3534c0ac6f2fd1dd90b8e651fc064d844f
Prasoonjha1 <prasoonjha2000@gmail.com>
2022-08-24 17:45:16 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
+/*function Navbar()
+{
+    return (
+        <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+          <a class="navbar-brand" href="#">Navigation</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav mr-auto">
+                  <li class="nav-item active">
+                      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="#">Market</a>
+                  </li>
+              </ul>
+          </div>
+        </nav>
+  )
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function MainComp()
+{
+    return (
+            <h1>I am learning, React!</h1>
+            )
+}*/
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const page = (
+    <div>
+        <img src="logo192.png" width="40px"></img>
+        <h1>Fun facts about React</h1>
+        <div >
+            <ul>
+                <li>
+                    Was released in 2013
+                </li>
+                <li>
+                    Was originally created by Jordan Walke
+                </li>
+                <li>
+                    Has well over 100k stars on Github
+                </li>
+                <li>
+                  Is maintained by facebook
+                </li>
+                <li>
+                  Powers thousands of enterprise apps, including mobile apps
+                </li>
+            </ul>
+        </div>
+    </div>
+)
+
+//document.getElementById("root").append(JSON.stringify(page))
+
+ReactDOM.render(
+              page,
+              document.getElementById("root"))
